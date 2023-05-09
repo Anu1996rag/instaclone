7a33b10cbe9d3716988d40a7ac52930f171e6af3
Anu1996rag <anuragpatils1996@gmail.com>
2023-05-09 15:36:35 +0530

    added test cases

--- instaclone/content/tests.py
+++ instaclone/content/tests.py
@@ -1,3 +1,143 @@
-from django.test import TestCase
+from rest_framework.reverse import reverse
+from rest_framework.test import APITestCase
+from rest_framework import status
+
+
+class AuthenticateBaseTestCase(APITestCase):
+    def authenticate(self):
+        self.client.post(reverse("create_user_api"), {
+            "username": "test",
+            "password": "password"
+        })
+
+        response = self.client.post(reverse("login_api"), {
+            "username": "test",
+            "password": "password"
+        })
+
+        token = response.data["access"]
+        self.client.credentials(HTTP_AUTHORIZATION=f"Bearer {token}")
+
 
 # Create your tests here.
+class UserPostCreateFeedTestCase(AuthenticateBaseTestCase):
+    def setUp(self) -> None:
+        self.url = reverse('user_post_view')
+
+    def test_list_posts(self):
+        self.authenticate()
+        response = self.client.get(self.url)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_create_posts(self):
+        self.authenticate()
+
+        test_data = {
+            "caption_text": "Testing data",
+            "location": "Test location",
+            "is_published": True
+        }
+        response = self.client.post(self.url, test_data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.data["caption_text"], "Testing data")
+        self.assertEqual(response.data["location"], "Test location")
+        self.assertEqual(response.data["is_published"], True)
+
+
+class UserPostDetailUpdateViewTestCase(AuthenticateBaseTestCase):
+    def setUp(self) -> None:
+        self.authenticate()
+
+        test_data = {
+            "caption_text": "Testing data",
+            "location": "Test location",
+            "is_published": True
+        }
+        self.client.post(reverse('user_post_view'), test_data)
+
+    def test_get_post(self):
+        self.authenticate()
+
+        response = self.client.get('/post/1/')
+
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_update_user_post(self):
+        self.authenticate()
+
+        test_data = {
+            "caption_text": "My First Post",
+            "location": "Kolhapur, Maharashtra",
+            "is_published": True
+        }
+        response = self.client.put('/post/2/', data=test_data)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data["caption_text"], "My First Post")
+
+
+class PostLikeViewSetTestCase(AuthenticateBaseTestCase):
+    def setUp(self) -> None:
+        self.authenticate()
+
+        test_data = {
+            "caption_text": "Testing data",
+            "location": "Test location",
+            "is_published": True
+        }
+        self.client.post(reverse('user_post_view'), test_data)
+
+    def test_list_post_likes(self):
+        self.authenticate()
+        response = self.client.get('/post/like/', data={"post_id": 1})
+        print(response.data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_like_a_post(self):
+        self.authenticate()
+        post_data = {
+            "post": 1
+        }
+        response = self.client.post("/post/like/", data=post_data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_remove_like_on_a_post(self):
+        self.authenticate()
+        post_data = {
+            "post": 1
+        }
+        response = self.client.delete("/post/like/", data=post_data)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+
+class PostCommentViewSetTestCase(AuthenticateBaseTestCase):
+    def setUp(self) -> None:
+        self.authenticate()
+
+        test_data = {
+            "caption_text": "Testing data",
+            "location": "Test location",
+            "is_published": True
+        }
+        self.client.post(reverse('user_post_view'), test_data)
+
+    def test_create_comment_on_a_post(self):
+        self.authenticate()
+
+        test_data = {
+            "post": 1,
+            "text": "Test comment !"
+        }
+
+        response = self.client.post('/post/comment/', data=test_data)
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.data["text"], "Test comment !")
+
+    def test_list_comments_on_a_post(self):
+        self.authenticate()
+
+        response = self.client.get('/post/comment/', data={"post_id": 1})
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
--- instaclone/users/tests.py
+++ instaclone/users/tests.py
@@ -1,3 +1,124 @@
-from django.test import TestCase
+from rest_framework.reverse import reverse
+from rest_framework.test import APITestCase
+from rest_framework import status
 
-# Create your tests here.
+
+class AuthenticateBaseTestCase(APITestCase):
+    def authenticate(self):
+        self.client.post(reverse("create_user_api"), {
+            "username": "test",
+            "password": "password"
+        })
+
+        response = self.client.post(reverse("login_api"), {
+            "username": "test",
+            "password": "password"
+        })
+
+        if not response.data.get("access"):
+            token = response.data.get("data").get("access")
+        else:
+            token = response.data.get("access")
+        self.client.credentials(HTTP_AUTHORIZATION=f"Bearer {token}")
+
+
+class CreateUserTestCase(AuthenticateBaseTestCase):
+    def test_add_user(self):
+        test_data = {
+            "username": "test",
+            "password": "test",
+            "email": "test@gmail.com"
+        }
+
+        response = self.client.post(reverse("create_user_api"), data=test_data)
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.data["errors"], "")
+
+
+class ListUsersTestCase(AuthenticateBaseTestCase):
+    def test_list_users(self):
+        self.authenticate()
+
+        response = self.client.get("/users/list/")
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+class UserProfileDetailTestCase(AuthenticateBaseTestCase):
+    def setUp(self):
+        self.authenticate()
+        test_data = {
+            "username": "test1",
+            "password": "test",
+            "email": "test@gmail.com"
+        }
+
+        self.client.post(reverse("create_user_api"), data=test_data)
+
+    def test_get_user_profile_detail(self):
+        self.authenticate()
+        response = self.client.get("/users/1/")
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_update_user_profile_detail(self):
+        self.authenticate()
+
+        test_data = {
+            "first_name": "test_updated_username",
+            "last_name": "test_updated_lastname",
+            "bio": "updated bio"
+        }
+        response = self.client.put("/users/1/", data=test_data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data["bio"], "updated bio")
+
+
+class UserNetworkEdgeViewTestCase(AuthenticateBaseTestCase):
+    def setUp(self):
+        self.url = reverse("user_network_edge_api")
+        self.authenticate()
+        test_data1 = {
+            "username": "test1",
+            "password": "test",
+            "email": "test1@gmail.com"
+        }
+
+        self.client.post(reverse("create_user_api"), data=test_data1)
+
+    def test_follow_a_user(self):
+        self.authenticate()
+        data = {
+            "to_user": 2
+        }
+        response = self.client.post(self.url, data, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_get_list_of_following(self):
+        self.authenticate()
+        data = {
+            "direction": "following"
+        }
+        response = self.client.get(self.url, data)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_get_list_of_followers(self):
+        self.authenticate()
+        data = {
+            "direction": "followers"
+        }
+        response = self.client.get(self.url, data)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_unfollow_user(self):
+        self.authenticate()
+        data = {
+             "to_user": 2
+        }
+        response = self.client.delete(self.url, data, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
